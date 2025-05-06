6f81e645363bb96b1a8de87daf41483bbf14f3c1
jozsi <jozsefkiss90@gmail.com>
2025-05-06 15:54:48 +0200

    preparing new redeploy

--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,8 @@ app.include_router(relationships.router)
 app.include_router(integrate.router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
-    #allow_origins=["https://knowledge-graph-frontend-production.up.railway.app"],
+    #allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["https://knowledge-graph-frontend-production.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
